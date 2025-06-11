07caace7fd82b826b2e5cf409c10ad3c4ffb65a1
Daníel Jóhannsson <daniel.johannsson1999@gmail.com>
2025-06-11 17:24:59 +0200

    Created accountControllers

---
+++ src/controllers/accountController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { accounts } from "../data";
+
+export const getAllAccounts = (req: Request, res: Response) => {
+  res.json(accounts);
+};
+
+export const getAccountById = (req: Request, res: Response) => {
+  const account = accounts.find((acc) => acc.accountNumber === req.params.id);
+  if (!account) {
+    return res.status(404).json({ error: "Account not found" });
+  }
+  res.json(account);
+};
