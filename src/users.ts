87f49f6a60a48dbb5b80cbc2c993cda955143459
Daníel Jóhannsson <daniel.johannsson1999@gmail.com>
2025-06-12 22:12:32 +0200

    Created user for auth

---
+++ src/users.ts
@@ -0,0 +1,8 @@
+export const users = [
+  {
+    id: "1",
+    username: "daniel",
+    passwordHash:
+      "$2a$12$A30IGk3wHEfMnO0zI5cunO2KhMQoT1hyEZBT7cZknWY0hqyLuE.Ia", // password: "password123"
+  },
+];
