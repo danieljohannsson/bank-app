319472e2986ef53c5fd43a0dd26fa904461049ca
Daníel Jóhannsson <daniel.johannsson1999@gmail.com>
2025-09-16 13:24:31 +0200

    old commit

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from "fs";
+import { readFileSync, stat } from "fs";
 import { XMLParser } from "fast-xml-parser";
 import { AccountInfo, Transaction } from "./types";
 
@@ -13,63 +13,115 @@ const statement = json.Document.BkToCstmrStmt.Stmt;
 
 export const accounts: AccountInfo[] = (
   Array.isArray(statement.Acct) ? statement.Acct : [statement.Acct]
-).map((ntry: any) => {
+).map((acct: any) => {
   const clbdBalance = statement.Bal.find(
     (bal: any) => bal.Tp?.CdOrPrtry?.Cd === "CLBD"
   );
   return {
-    id: String(ntry.Id.Othr.Id),
-    currency: ntry.Ccy,
-    ownerName: ntry.Ownr.Nm,
+    id: String(acct.Id.Othr.Id),
+    currency: acct.Ccy,
+    ownerName: acct.Ownr.Nm,
     balance: String(clbdBalance?.CdtLine?.Amt["#text"]) ?? "0",
   };
 });
 
-export const transactions: Transaction[] = (
-  Array.isArray(statement.Ntry) ? statement.Ntry : [statement.Ntry]
-).map((ntry: any) => {
-  const details = ntry.NtryDtls.TxDtls;
-  console.log("details:", details, Array.isArray(details));
-  return {
-    bookingDate: ntry.BookgDt.Dt,
-    amount: ntry.Amt["#text"],
-    currency: ntry.Amt["@_Ccy"],
-    remittanceInfo: Array.isArray(details)
-      ? details.map((dtls: any) => {
-          const strd = dtls.RmtInf?.Strd;
-          if (strd) {
-            const rmtdAmt = strd.RfrdDocAmt?.RmtdAmt;
-            const cdtrRef = strd.CdtrRefInf?.Ref;
-            const cd = strd.RfrdDocInf?.Tp?.CdOrPrtry?.Cd;
-            const nbr = strd.RfrdDocInf?.Nb;
-            return {
-              amount: rmtdAmt?.["#text"] ?? null,
-              currency: rmtdAmt?.["@_Ccy"] ?? null,
-              creditorReference: cdtrRef ?? null,
-              referredDocInfo: {
-                code: cd ?? null,
-                number: nbr ?? null,
-              },
-            };
-          }
-          return { info: "no-info" };
-        })
-      : details.RmtInf?.Strd
-      ? {
-          amount: details.RmtInf.Strd.RfrdDocAmt?.RmtdAmt?.["#text"] ?? null,
-          currency: details.RmtInf.Strd.RfrdDocAmt?.RmtdAmt?.["@_Ccy"] ?? null,
-          creditorReference: details.RmtInf.Strd.CdtrRefInf?.Ref ?? null,
-          referredDocInfo: {
-            code: details.RmtInf.Strd.RfrdDocInf?.Tp?.CdOrPrtry?.Cd ?? null,
-            number: details.RmtInf.Strd.RfrdDocInf?.Nb ?? null,
-          },
-        }
-      : details.RmtInf?.Ustrd
-      ? { unstructured: details.RmtInf?.Ustrd }
-      : { info: "no-info" },
-  };
-});
-
-console.log(JSON.stringify(transactions, null, 2));
+export const transactions: Transaction[] = Array.isArray(statement)
+  ? statement.flatMap((stmt: any) =>
+      (Array.isArray(stmt.Ntry) ? stmt.Ntry : [stmt.Ntry]).map((ntry: any) => {
+        const details = ntry.NtryDtls.TxDtls;
+        return {
+          bookingDate: ntry.BookgDt.Dt,
+          amount: ntry.Amt["#text"],
+          currency: ntry.Amt["@_Ccy"],
+          remittanceInfo: Array.isArray(details)
+            ? details.map((dtls: any) => {
+                const strd = dtls.RmtInf?.Strd;
+                if (strd) {
+                  const rmtdAmt = strd.RfrdDocAmt?.RmtdAmt;
+                  const cdtrRef = strd.CdtrRefInf?.Ref;
+                  const cd = strd.RfrdDocInf?.Tp?.CdOrPrtry?.Cd;
+                  const nbr = strd.RfrdDocInf?.Nb;
+                  return {
+                    amount: rmtdAmt?.["#text"] ?? null,
+                    currency: rmtdAmt?.["@_Ccy"] ?? null,
+                    creditorReference: cdtrRef ?? null,
+                    referredDocInfo: {
+                      code: cd ?? null,
+                      number: nbr ?? null,
+                    },
+                  };
+                }
+                return { info: "no-info" };
+              })
+            : details.RmtInf?.Strd
+            ? {
+                amount:
+                  details.RmtInf.Strd.RfrdDocAmt?.RmtdAmt?.["#text"] ?? null,
+                currency:
+                  details.RmtInf.Strd.RfrdDocAmt?.RmtdAmt?.["@_Ccy"] ?? null,
+                creditorReference: details.RmtInf.Strd.CdtrRefInf?.Ref ?? null,
+                referredDocInfo: {
+                  code:
+                    details.RmtInf.Strd.RfrdDocInf?.Tp?.CdOrPrtry?.Cd ?? null,
+                  number: details.RmtInf.Strd.RfrdDocInf?.Nb ?? null,
+                },
+              }
+            : details.RmtInf?.Ustrd
+            ? {
+                unstructured: details.RmtInf?.Ustrd,
+              }
+            : { info: "no-info" },
+        };
+      })
+    )
+  : (Array.isArray(statement.Ntry) ? statement.Ntry : [statement.Ntry]).map(
+      (ntry: any) => {
+        const details = ntry.NtryDtls.TxDtls;
+        return {
+          bookingDate: ntry.BookgDt.Dt,
+          amount: ntry.Amt["#text"],
+          currency: ntry.Amt["@_Ccy"],
+          remittanceInfo: Array.isArray(details)
+            ? details.map((dtls: any) => {
+                const strd = dtls.RmtInf?.Strd;
+                if (strd) {
+                  const rmtdAmt = strd.RfrdDocAmt?.RmtdAmt;
+                  const cdtrRef = strd.CdtrRefInf?.Ref;
+                  const cd = strd.RfrdDocInf?.Tp?.CdOrPrtry?.Cd;
+                  const nbr = strd.RfrdDocInf?.Nb;
+                  return {
+                    amount: rmtdAmt?.["#text"] ?? null,
+                    currency: rmtdAmt?.["@_Ccy"] ?? null,
+                    creditorReference: cdtrRef ?? null,
+                    referredDocInfo: {
+                      code: cd ?? null,
+                      number: nbr ?? null,
+                    },
+                  };
+                }
+                return { info: "no-info" };
+              })
+            : details.RmtInf?.Strd
+            ? {
+                amount:
+                  details.RmtInf.Strd.RfrdDocAmt?.RmtdAmt?.["#text"] ?? null,
+                currency:
+                  details.RmtInf.Strd.RfrdDocAmt?.RmtdAmt?.["@_Ccy"] ?? null,
+                creditorReference: details.RmtInf.Strd.CdtrRefInf?.Ref ?? null,
+                referredDocInfo: {
+                  code:
+                    details.RmtInf.Strd.RfrdDocInf?.Tp?.CdOrPrtry?.Cd ?? null,
+                  number: details.RmtInf.Strd.RfrdDocInf?.Nb ?? null,
+                },
+              }
+            : details.RmtInf?.Ustrd
+            ? {
+                unstructured: details.RmtInf?.Ustrd,
+              }
+            : { info: "no-info" },
+        };
+      }
+    );
+console.log("transactions", JSON.stringify(transactions, null, 2));
 console.log(JSON.stringify(accounts, null, 2));
 console.log(statement.Ntry[0].NtryDtls.TxDtls[0].RmtInf.Strd);
